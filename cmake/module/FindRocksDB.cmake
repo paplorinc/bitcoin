f2bffa0ad6dd8d77540b3c5c1a901612c241ab5c
Lőrinc <pap.lorinc@gmail.com>
2024-10-03 11:03:28 +0200

    Add RocksDB sources inside src/rocksdb

--- cmake/module/FindRocksDB.cmake
+++ cmake/module/FindRocksDB.cmake
@@ -2,27 +2,35 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit/.
 
-# Find the RocksDB headers and libraries
-find_path(ROCKSDB_INCLUDE_DIR NAMES rocksdb/db.h)
-find_library(ROCKSDB_LIBRARY NAMES rocksdb)
+# Define paths for RocksDB sources and build directories
+set(ROCKSDB_SOURCE_DIR "${CMAKE_SOURCE_DIR}/src/rocksdb")
+set(ROCKSDB_BUILD_DIR "${CMAKE_BINARY_DIR}/rocksdb_build")
 
-# Standard CMake helper to ensure the libraries and headers are found
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(RocksDB
-  REQUIRED_VARS ROCKSDB_LIBRARY ROCKSDB_INCLUDE_DIR
+# Specify output for RocksDB libraries and include directories
+set(ROCKSDB_INCLUDE_DIR "${ROCKSDB_SOURCE_DIR}/include")
+set(ROCKSDB_LIBRARY "${ROCKSDB_BUILD_DIR}/librocksdb.a")
+
+# Create a custom command to build RocksDB separately and copy the library to the build directory
+add_custom_command(
+    OUTPUT ${ROCKSDB_LIBRARY}
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${ROCKSDB_BUILD_DIR}
+    COMMAND USE_RTTI=1 ROCKSDB_DISABLE_SNAPPY=1 ROCKSDB_DISABLE_ZLIB=1 ROCKSDB_DISABLE_BZIP=1 ROCKSDB_DISABLE_LZ4=1 ROCKSDB_DISABLE_ZSTD=1 make static_lib -C ${ROCKSDB_SOURCE_DIR} -j$$(nproc)
+    COMMAND ${CMAKE_COMMAND} -E copy ${ROCKSDB_SOURCE_DIR}/librocksdb.a ${ROCKSDB_BUILD_DIR}/librocksdb.a
+    WORKING_DIRECTORY ${ROCKSDB_SOURCE_DIR}
+    COMMENT "Building and copying RocksDB static library"
 )
 
-# If RocksDB was found and no existing target is defined, define a new imported target
-if(ROCKSDB_FOUND AND NOT TARGET RocksDB::RocksDB)
-  add_library(RocksDB::RocksDB STATIC IMPORTED)
-  set_target_properties(RocksDB::RocksDB PROPERTIES
-      IMPORTED_LOCATION "${ROCKSDB_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${ROCKSDB_INCLUDE_DIR}"
-  )
-endif()
-
-# Mark the include and library paths as advanced to hide them in the CMake GUI by default
-mark_as_advanced(
-  ROCKSDB_INCLUDE_DIR
-  ROCKSDB_LIBRARY
+# Add a custom target for RocksDB that depends on the library
+add_custom_target(build_rocksdb ALL
+    DEPENDS ${ROCKSDB_LIBRARY}
 )
+
+# Define RocksDB as an imported target to be used in the rest of the project
+add_library(RocksDB::RocksDB STATIC IMPORTED GLOBAL)
+set_target_properties(RocksDB::RocksDB PROPERTIES
+    IMPORTED_LOCATION ${ROCKSDB_LIBRARY}
+    INTERFACE_INCLUDE_DIRECTORIES ${ROCKSDB_INCLUDE_DIR}
+)
+
+# Ensure that the RocksDB target is built before the rest of the project
+add_dependencies(RocksDB::RocksDB build_rocksdb)
