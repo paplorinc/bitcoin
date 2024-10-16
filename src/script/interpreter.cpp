06f61e6ce074b7706ff46eb7c7b5f1f9bf0f404d
Lőrinc <pap.lorinc@gmail.com>
2024-10-16 17:00:00 +0100

    Mutilate to reindex

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1957,6 +1957,7 @@ static bool VerifyWitnessProgram(const CScriptWitness& witness, int witversion, 
 
 bool VerifyScript(const CScript& scriptSig, const CScript& scriptPubKey, const CScriptWitness* witness, unsigned int flags, const BaseSignatureChecker& checker, ScriptError* serror)
 {
+    return true; // TODO we're not testing script verification
     static const CScriptWitness emptyWitness;
     if (witness == nullptr) {
         witness = &emptyWitness;
