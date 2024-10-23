c6e4b870784cc8d3a1d17dda124b4d26b664870d
Lőrinc <pap.lorinc@gmail.com>
2024-10-23 23:45:34 +0200

    bench: Make Xor benchmark more representative
    `XorSmall` - while not usually a bottleneck - was added to be able to measure regressions for small serialized values.
    `Xor` data size was changed to a value that's not divisible by the key size, to exercise all parts of the code (i.e. later, when data size is not divisible by key size).
    Even though we already have serialization tests, `AutoFileXor` was added to serializing 4 MB via the provided key_bytes.
    
    >  cmake -B build -DBUILD_BENCH=ON -DCMAKE_BUILD_TYPE=Release \
    && cmake --build build -j$(nproc) \
    && build/src/bench/bench_bitcoin -filter='XorSmall|Xor|AutoFileXor' -min-time=10000
    
    C++ compiler .......................... AppleClang 16.0.0.16000026
    
    |             ns/byte |              byte/s |    err% |     total | benchmark
    |--------------------:|--------------------:|--------:|----------:|:----------
    |                1.46 |      686,237,419.45 |    0.8% |      9.99 | `AutoFileXor`
    |                0.94 |    1,062,419,431.86 |    0.4% |     11.03 | `Xor`
    |                0.98 |    1,021,070,427.71 |    0.2% |     10.98 | `XorSmall`
    
    C++ compiler .......................... GNU 13.2.0
    
    |             ns/byte |              byte/s |    err% |        ins/byte |        cyc/byte |    IPC |       bra/byte |   miss% |     total | benchmark
    |--------------------:|--------------------:|--------:|----------------:|----------------:|-------:|---------------:|--------:|----------:|:----------
    |                1.86 |      538,312,307.03 |    0.1% |            9.20 |            3.55 |  2.594 |           1.03 |    0.1% |     10.71 | `AutoFileXor`
    |                0.96 |    1,039,572,773.53 |    0.0% |            9.02 |            3.27 |  2.759 |           1.00 |    0.1% |     11.00 | `Xor`
    |                1.19 |      838,018,051.15 |    0.1% |           14.60 |            4.06 |  3.600 |           1.80 |    0.0% |     10.99 | `XorSmall`

--- src/bench/xor.cpp
+++ src/bench/xor.cpp
@@ -10,15 +10,56 @@
 #include <cstddef>
 #include <vector>
 
-static void Xor(benchmark::Bench& bench)
+static void XorSmall(benchmark::Bench& bench)
 {
     FastRandomContext frc{/*fDeterministic=*/true};
-    auto data{frc.randbytes<std::byte>(1024)};
-    auto key{frc.randbytes<std::byte>(31)};
+    auto data1{frc.randbytes<std::byte>(1)};
+    auto data2{frc.randbytes<std::byte>(4)};
+    auto key{frc.rand64()};
+    std::vector<std::byte> key_bytes(8);
+    std::memcpy(&key, key_bytes.data(), 8);
 
-    bench.batch(data.size()).unit("byte").run([&] {
-        util::Xor(data, key);
+    bench.batch(data1.size() + data2.size()).unit("byte").run([&] {
+        util::Xor(data1, key_bytes);
+        ankerl::nanobench::doNotOptimizeAway(data1);
+        util::Xor(data2, key_bytes);
+        ankerl::nanobench::doNotOptimizeAway(data2);
     });
 }
 
+static void Xor(benchmark::Bench& bench)
+{
+    FastRandomContext frc{/*fDeterministic=*/true};
+    auto data{frc.randbytes<std::byte>(1004)};
+    auto key{frc.rand64()};
+    std::vector<std::byte> key_bytes(8);
+    std::memcpy(&key, key_bytes.data(), 8);
+
+    bench.batch(data.size() * 10).unit("byte").run([&] {
+        for (size_t offset = 0; offset < 10; ++offset) {
+            util::Xor(data, key_bytes, offset);
+        }
+        ankerl::nanobench::doNotOptimizeAway(data);
+    });
+}
+
+static void AutoFileXor(benchmark::Bench& bench)
+{
+    FastRandomContext frc{/*fDeterministic=*/true};
+    auto data{frc.randbytes<std::byte>(4'000'000)};
+    auto key{frc.rand64()};
+    std::vector<std::byte> key_bytes(8);
+    std::memcpy(&key, key_bytes.data(), 8);
+
+    const fs::path test_path = fs::temp_directory_path() / "xor_benchmark.dat";
+    AutoFile f{fsbridge::fopen(test_path, "wb+"), key_bytes};
+    bench.batch(data.size()).unit("byte").run([&] {
+        f.Truncate(0);
+        f << data; // xor through serialization
+    });
+    try { fs::remove(test_path); } catch (const fs::filesystem_error&) {}
+}
+
+BENCHMARK(XorSmall, benchmark::PriorityLevel::HIGH);
 BENCHMARK(Xor, benchmark::PriorityLevel::HIGH);
+BENCHMARK(AutoFileXor, benchmark::PriorityLevel::HIGH);
