d9cc97efe55412193f76128dd515453f6eac80a0
Lőrinc <pap.lorinc@gmail.com>
2024-11-02 20:58:03 +0100

    write_buffer_size = 2 << 20

--- src/dbwrapper.cpp
+++ src/dbwrapper.cpp
@@ -138,7 +138,7 @@ static leveldb::Options GetOptions(size_t nCacheSize)
 {
     leveldb::Options options;
     options.block_cache = leveldb::NewLRUCache(1 << 20);
-    options.write_buffer_size = 512 << 10; // up to two write buffers may be held in memory simultaneously
+    options.write_buffer_size = 2 << 20; // up to two write buffers may be held in memory simultaneously
     options.max_file_size = 16 << 20;
     options.filter_policy = leveldb::NewBloomFilterPolicy(10);
     options.compression = leveldb::kNoCompression;
