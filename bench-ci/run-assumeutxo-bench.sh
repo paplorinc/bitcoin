38411e34fb3437f689affb9162cca5bbbb6ac8c7
willcl-ark <will@256k1.dev>
2025-01-06 18:53:54 +0000

    fixup typo in bench script

--- bench-ci/run-assumeutxo-bench.sh
+++ bench-ci/run-assumeutxo-bench.sh
@@ -78,7 +78,7 @@ prepare_assumeutxo_snapshot_run() {
   # Run the actual preparation steps
   clean_datadir "${TMP_DATADIR}"
   taskset -c 0-15 build/src/bitcoind -datadir="${TMP_DATADIR}" -connect="${CONNECT_ADDRESS}" -daemon=0 -chain="${CHAIN}" -stopatheight=1 -printtoconsole=0
-  taskset -c 0-15 build/src/bitcoind -datadir="${TMP_DATADIR}" -connect="${CONNECT_ADDRESS}" -daemon=0 -chain="${CHAIN}" -dbcache="${DBCACHE} -pausebackgroundsync=1 -loadutxosnapshot="${UTXO_PATH}" -printtoconsole=0 || true
+  taskset -c 0-15 build/src/bitcoind -datadir="${TMP_DATADIR}" -connect="${CONNECT_ADDRESS}" -daemon=0 -chain="${CHAIN}" -dbcache="${DBCACHE}" -pausebackgroundsync=1 -loadutxosnapshot="${UTXO_PATH}" -printtoconsole=0 || true
   clean_logs "${TMP_DATADIR}"
 }
 
