85774fe88f6690b2bdac81abc7f8c7b6e864979a
willcl-ark <will@256k1.dev>
2025-01-07 07:50:10 +0000

    streamline shell.nix

--- shell.nix
+++ shell.nix
@@ -1,18 +1,11 @@
 # Copyright 0xB10C, willcl-ark
-{ pkgs ? import (fetchTarball "https://github.com/nixos/nixpkgs/archive/nixos-24.11.tar.gz") {},
-  spareCores ? 0,
-  withClang ? false,
-  withDebug ? false,
-}:
+{ pkgs ? import
+  (fetchTarball "https://github.com/nixos/nixpkgs/archive/nixos-24.11.tar.gz")
+  { }, }:
 let
   inherit (pkgs.lib) optionals strings;
   inherit (pkgs) stdenv;
 
-  # Add mlc binary fetching
-  mlcBinary = pkgs.fetchurl {
-    url = "https://github.com/becheran/mlc/releases/download/v0.18.0/mlc-x86_64-linux";
-    sha256 = "sha256-jbdp+UlFybBE+o567L398hbcWHsG8aQGqYYf5h9JRkw=";
-  };
   # Hyperfine
   # Included here because we need master for the `--conclude` flag from pr 719
   hyperfine = pkgs.rustPlatform.buildRustPackage rec {
@@ -38,111 +31,47 @@ let
     };
   };
 
-  # Create a derivation for mlc
-  mlc = pkgs.runCommand "mlc" {} ''
-    mkdir -p $out/bin
-    cp ${mlcBinary} $out/bin/mlc
-    chmod +x $out/bin/mlc
-  '';
-
-  binDirs =
-    [ "\$PWD/build/src" ];
-  configureFlags =
-    [ "--with-boost-libdir=$NIX_BOOST_LIB_DIR" ]
-    ++ optionals withClang [ "CXX=clang++" "CC=clang" ]
-    ++ optionals withDebug [ "--enable-debug" ];
-  jobs =
-    if (strings.hasSuffix "linux" builtins.currentSystem) then "$(($(nproc)-${toString spareCores}))"
-    else if (strings.hasSuffix "darwin" builtins.currentSystem) then "$(($(sysctl -n hw.physicalcpu)-${toString spareCores}))"
-    else "6";
 in pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [
-      autoconf
-      automake
-      libtool
-      pkg-config
-      boost
-      libevent
-      zeromq
-      sqlite
-      clang_18
+  nativeBuildInputs = with pkgs; [
+    autoconf
+    automake
+    boost
+    ccache
+    clang_18
+    cmake
+    libevent
+    libtool
+    pkg-config
+    sqlite
+    zeromq
+  ];
+  buildInputs = with pkgs; [
+    just
+    bash
+    git
+    shellcheck
+    python310
+    uv
 
-      # tests
-      hexdump
+    # Benchmarking
+    flamegraph
+    hyperfine
+    jq
+    linuxKernel.packages.linux_6_6.perf
+    perf-tools
+    util-linux
+  ];
 
-      # compiler output caching per
-      # https://github.com/bitcoin/bitcoin/blob/master/doc/productivity.md#cache-compilations-with-ccache
-      ccache
+  shellHook = ''
+    echo "Bitcoin Core build nix-shell"
+    echo ""
+    echo "Setting up python venv"
 
-      # for newer cmake building
-      cmake
+    # fixes libstdc++ issues and libgl.so issues
+    export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:$LD_LIBRARY_PATH
 
-      # depends
-      byacc
-
-      # debugging
-      gdb
-
-      # tracing
-      libsystemtap
-      linuxPackages.bpftrace
-      linuxPackages.bcc
-
-    ];
-    buildInputs = with pkgs; [
-      just
-      bash
-
-      # lint requirements
-      cargo
-      git
-      mlc
-      ruff
-      rustc
-      rustup
-      shellcheck
-      python310
-      uv
-
-      # Benchmarking
-      flamegraph
-      hyperfine
-      jq
-      linuxKernel.packages.linux_6_6.perf
-      perf-tools
-      util-linux
-    ];
-
-    # Modifies the Nix clang++ wrapper to avoid warning:
-    # "_FORTIFY_SOURCE requires compiling with optimization (-O)"
-    hardeningDisable = if withDebug then [ "all" ] else [ ];
-
-    shellHook = ''
-      echo "Bitcoin Core build nix-shell"
-      echo ""
-      echo "Setting up python venv"
-
-      # fixes libstdc++ issues and libgl.so issues
-      export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:$LD_LIBRARY_PATH
-
-      uv venv --python 3.10
-      source .venv/bin/activate
-      uv pip install -r pyproject.toml
-
-      BCC_EGG=${pkgs.linuxPackages.bcc}/${pkgs.python3.sitePackages}/bcc-${pkgs.linuxPackages.bcc.version}-py3.${pkgs.python3.sourceVersion.minor}.egg
-
-      echo "adding bcc egg to PYTHONPATH: $BCC_EGG"
-      if [ -f $BCC_EGG ]; then
-        export PYTHONPATH="$PYTHONPATH:$BCC_EGG"
-        echo ""
-      else
-        echo "The bcc egg $BCC_EGG does not exist. Maybe the python or bcc version is different?"
-      fi
-
-      echo "adding ${builtins.concatStringsSep ":" binDirs} to \$PATH to make running built binaries more natural"
-      export PATH=$PATH:${builtins.concatStringsSep ":" binDirs};
-
-      rustup default stable
-      rustup component add rustfmt
-    '';
+    uv venv --python 3.10
+    source .venv/bin/activate
+    uv pip install -r pyproject.toml
+  '';
 }
